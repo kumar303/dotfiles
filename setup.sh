10e79f98ad41c7ec867f9742fa3f64959017d914
Kumar McMillan <kumar.mcmillan@shopify.com>
2021-08-17 17:03:08 -0500

    Add a setup file for spin

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This file is run by Spin https://development.shopify.io/engineering/keytech/spin/personalizing
+
+if [ $SPIN ]; then
+  echo "[kumar's dotfiles]: bootstrapping spin"
+  if [ -d /src/github.com/shopify/checkout-web/.vscode/tasks.json ]; then
+    echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
+    ln -sf ~/dotfiles/shopify/spin/checkout-web/.vscode/tasks.json /src/github.com/shopify/checkout-web/.vscode/tasks.json
+  fi
+fi
