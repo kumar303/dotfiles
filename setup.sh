615496194ab0d905acbefb2fe244ec8c49a93d7f
Kumar McMillan <kumar.mcmillan@shopify.com>
2021-08-19 16:16:37 -0500

    Pimping out spin a bit more

--- setup.sh
+++ setup.sh
@@ -4,8 +4,17 @@
 
 if [ $SPIN ]; then
   echo "[kumar's dotfiles]: bootstrapping spin"
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.ci commit
+  git config --global alias.st status
+
   if [ -d /src/github.com/shopify/checkout-web/ ]; then
     echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
     ln -sf ~/dotfiles/shopify/spin/checkout-web/.vscode/tasks.json /src/github.com/shopify/checkout-web/.vscode/tasks.json
+  fi
+  if [ -d /src/github.com/shopify/shopify/ ]; then
+    echo "[kumar's dotfiles]: adding some betas"
+    SHOP_ID=1 BETA=force_checkout_one bin/rake dev:betas:enable
   fi
 fi
