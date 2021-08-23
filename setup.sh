c379d66ca0e2538a4a5b94da1df3d4d8e04c242c
Kumar McMillan <kumar.mcmillan@shopify.com>
2021-08-23 11:37:23 -0500

    Install gh on spin

--- setup.sh
+++ setup.sh
@@ -9,6 +9,12 @@ if [ $SPIN ]; then
   git config --global alias.ci commit
   git config --global alias.st status
 
+  # Install gh according to https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  sudo apt update
+  sudo apt install gh
+
   if [ -d /src/github.com/shopify/checkout-web/ ]; then
     echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
     ln -sf ~/dotfiles/shopify/spin/checkout-web/.vscode/tasks.json /src/github.com/shopify/checkout-web/.vscode/tasks.json
