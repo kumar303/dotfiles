d87a6d578bd1010b6881adc8347bda0879ea0187
Kumar McMillan <kumar.mcmillan@shopify.com>
2021-11-08 08:25:32 -0600

    Better spin init, remove redundant gh install

--- setup.sh
+++ setup.sh
@@ -18,15 +18,10 @@ if [ $SPIN ]; then
 
   cat ~/dotfiles/shopify/spin/.zshrc >> ~/.zshrc
 
-  # Install gh according to https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-  sudo apt update
-  sudo apt install gh
-
   if [ -d /src/github.com/shopify/checkout-web/ ]; then
     echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
-    ln -sf ~/dotfiles/shopify/spin/checkout-web/.vscode/tasks.json /src/github.com/shopify/checkout-web/.vscode/tasks.json
+    cd /src/github.com/shopify/checkout-web/
+    yarn init-vscode
   fi
   if [ -d /src/github.com/shopify/shopify/ ]; then
     echo "[kumar's dotfiles]: adding some betas"
