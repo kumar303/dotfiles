0956b387ee13351315f489c19f29ea8cafcd2685
Kumar McMillan <kumar.mcmillan@shopify.com>
2021-11-16 09:26:49 -0600

    Remove isospin bootstrapping

--- setup.sh
+++ setup.sh
@@ -15,25 +15,32 @@ if [ $SPIN ]; then
   git config --global alias.pp "\!git remote prune origin && git pull"
   git config --global alias.ri "rebase --interactive --autosquash HEAD~5"
   git config --global alias.st status
+  git config --global core.editor vim
 
   cat ~/dotfiles/shopify/spin/.zshrc >> ~/.zshrc
 
   if [ -d /src/github.com ]; then
     # We're on legacy spin
     SRC=/src/github.com/shopify
+    LEGACY=1
   else
     # We're on isospin
     SRC=/home/spin/src/github.com/Shopify
+    LEGACY=0
   fi
 
-  if [ -d $SRC/checkout-web/ ]; then
-    echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
-    cd $SRC/checkout-web/
-    yarn init-vscode
-  fi
-  if [ -d $SRC/shopify/ ]; then
-    echo "[kumar's dotfiles]: adding some betas"
-    cd $SRC/shopify/
-    SHOP_ID=1 BETA=force_checkout_one ./bin/rake dev:betas:enable
+  if [ $LEGACY -eq "1" ]; then
+    # This cannot be run on isospin because of timing issues.
+    # See https://github.com/Shopify/spin/issues/3088
+    if [ -d $SRC/checkout-web/ ]; then
+      echo "[kumar's dotfiles]: adding tasks.json to checkout-web"
+      cd $SRC/checkout-web/
+      yarn init-vscode
+    fi
+    if [ -d $SRC/shopify/ ]; then
+      echo "[kumar's dotfiles]: adding some betas"
+      cd $SRC/shopify/
+      SHOP_ID=1 BETA=force_checkout_one ./bin/rake dev:betas:enable
+    fi
   fi
 fi
