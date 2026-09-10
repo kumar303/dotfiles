9fc6388a17641a164257669144d2c3fa19dd5643
Kumar McMillan <kumar.mcmillan@shopify.com>
2026-09-10 23:36:20 +0100

    Remove setup migrations

--- setup.sh
+++ setup.sh
@@ -36,32 +36,10 @@ link_file() {
   echo "Linked: $dest -> $src"
 }
 
-legacy_vim="$HOME/.vim"
-legacy_vim_target="$repo_dir/.vim"
-if [ -L "$legacy_vim" ] && [ "$(readlink "$legacy_vim")" = "$legacy_vim_target" ]; then
-  rm "$legacy_vim"
-  echo "Removed legacy link: $legacy_vim"
-fi
-
-legacy_import_resolver="$HOME/.vim/import-resolver.js"
-legacy_import_resolver_target="$dotfiles_dir/.vim/import-resolver.js"
-if [ -L "$legacy_import_resolver" ] && [ "$(readlink "$legacy_import_resolver")" = "$legacy_import_resolver_target" ]; then
-  rm "$legacy_import_resolver"
-  echo "Removed legacy link: $legacy_import_resolver"
-fi
-
 while IFS= read -r -d '' -u 3 src; do
   relative_path="${src#"$dotfiles_dir"/}"
   link_file "$src" "$HOME/$relative_path"
 done 3< <(find "$dotfiles_dir" -type f -print0)
-
-legacy_script="$HOME/.config/herdr/scripts/split-vim-above.js"
-legacy_target="$HOME/src/github.com/kumar303/herdr-config/dot-config/herdr/scripts/split-vim-above.js"
-if [ -L "$legacy_script" ] && [ "$(readlink "$legacy_script")" = "$legacy_target" ]; then
-  rm "$legacy_script"
-  rmdir "$(dirname "$legacy_script")" 2>/dev/null || true
-  echo "Removed legacy link: $legacy_script"
-fi
 
 echo "Linking Herdr plugins"
 for plugin_dir in "$repo_dir"/plugins/*; do
